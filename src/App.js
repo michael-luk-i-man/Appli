d23a992f8edea5f2a1317d35416d530a6425d7e6
armisticeml <luck.michael@nyu.edu>
2016-12-28 19:24:48 -0800

    Implement MVP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Me from './components/Me'
 
 class App extends Component {
   render() {
@@ -11,8 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <Me />       </p>
       </div>
     );
   }
