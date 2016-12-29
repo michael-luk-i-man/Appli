7ded3386228c8104184ba2aa8cea9a58ac9680c6
armisticeml <luck.michael@nyu.edu>
2016-12-28 19:43:30 -0800

    Implement MVP

--- src/components/Schools.js
+++ src/components/Schools.js
@@ -7,7 +7,7 @@ class Me extends Component {
         <h1>My Schools</h1>
         <img src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-1/c72.0.576.576/s160x160/13466518_1158176530901917_5405760124033243530_n.jpg?oh=f239b9f0f254d98a5cb5d170f8575dab&oe=58EA3D2C"></img>
         <h2>Dolan Darma</h2>
-        <h3>CSUN, Electrical Engineering 2018</h3>
+        <h3>California State University Northridge, Electrical Engineering 2018</h3>
         
       </div>
     );
